5592908779a5ab7ebf08f648f5d4fb7c181e7391
Piotr Kowalczuk <p.kowalczuk@travelaudience.com>
2017-05-23 21:27:00 +0200

    pypi integration

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+from subprocess import check_output
+
+with open('VERSION.txt', 'r') as content_file:
+    version = content_file.read()
+
+    setup(
+        name='ntypes',
+        version=version[1:],
+        description='ntypes data structures',
+        url='http://github.com/piotrkowalczuk/ntypes',
+        author='Piotr Kowalczuk',
+        author_email='p.kowalczuk.priv@gmail.com',
+        license='MIT',
+        packages=[''],
+        install_requires=[
+            'protobuf'
+        ],
+        zip_safe=False,
+        keywords=['protobuf', 'data-structures'],
+        download_url='https://github.com/piotrkowalczuk/ntypes/archive/%s.tar.gz' % version.rstrip(),
+      )
