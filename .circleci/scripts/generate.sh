dd7b7503b364baed5a4f2b4e095ca79d1c33eed8
Piotr Kowalczuk <p.kowalczuk.priv@gmail.com>
2018-05-06 20:57:09 +0200

    attempt #1

--- .circleci/scripts/generate.sh
+++ .circleci/scripts/generate.sh
@@ -1,12 +1,15 @@
 : ${PROTOC:="/usr/local/bin/protoc"}
 PROTO_INCLUDE="-I=/usr/include -I=."
 
+DIR_JAVA="./tmp/java/"
+DIR_PYTHON="./"
+
 case $1 in
     lint)
         ${PROTOC} ${PROTO_INCLUDE} --lint_out=. *.proto
         ;;
     python)
-        python -m grpc_tools.protoc ${PROTO_INCLUDE} --python_out=./ntypes *.proto
+        python -m grpc_tools.protoc ${PROTO_INCLUDE} --python_out=${DIR_PYTHON}ntypes *.proto
         ;;
     java)
         rm -rf ./tmp/java
