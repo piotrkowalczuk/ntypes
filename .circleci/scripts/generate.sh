5762932c1855179bf1667abd2b4c2841d781beaa
Piotr Kowalczuk <p.kowalczuk.priv@gmail.com>
2018-10-09 19:18:58 +0200

    jar file extended with some meta information (POC)

--- .circleci/scripts/generate.sh
+++ .circleci/scripts/generate.sh
@@ -1,6 +1,24 @@
 : ${PROTOC:="/usr/local/bin/protoc"}
 : ${SCALAPBC:="./tmp/scalapbc/scalapbc-0.8.1/bin/scalapbc"}
 PROTO_INCLUDE="-I=/usr/include -I=."
+VERSION=$(git describe --tags --always --dirty)
+POM_FILE="<?xml version=\"1.0\" encoding=\"UTF-8\"?> \
+<project xmlns=\"http://maven.apache.org/POM/4.0.0\" \
+         xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" \
+         xsi:schemaLocation=\"http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd\"> \
+    <modelVersion>4.0.0</modelVersion> \
+    <groupId>piotrkowalczuk</groupId> \
+    <artifactId>ntypes</artifactId> \
+    <version>${VERSION}</version> \
+    <dependencies> \
+        <!-- https://mvnrepository.com/artifact/com.thesamet.scalapb/compilerplugin --> \
+        <dependency> \
+            <groupId>com.thesamet.scalapb</groupId> \
+            <artifactId>compilerplugin_2.12</artifactId> \
+            <version>0.8.1</version> \
+        </dependency> \
+    </dependencies> \
+</project>"
 
 DIR_JAVA="./tmp/java/"
 DIR_PYTHON="./"
@@ -14,8 +32,12 @@ case $1 in
         ;;
     scala)
         rm -rf ./tmp/scala
-        mkdir -p ./tmp/scala
+        mkdir -p ./tmp/scala ./tmp/protobuf
         ${SCALAPBC} -v361 --scala_out=./tmp/scala/ntypes.jar ${PROTO_INCLUDE} *.proto
+        zip -ur ./tmp/scala/ntypes.jar ./README.md ./LICENSE.txt
+        echo "${POM_FILE}" > ./tmp/pom.xml
+        cp ntypes.proto ./tmp/protobuf/ntypes.proto
+        cd ./tmp/ && zip -ur ./scala/ntypes.jar ./pom.xml ./protobuf/ntypes.proto
         ;;
     golang | go)
         ${PROTOC} ${PROTO_INCLUDE} --go_out=${GOPATH}/src *.proto
