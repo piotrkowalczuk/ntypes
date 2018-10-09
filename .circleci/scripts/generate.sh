6b6d128306f6412a7caaf614f5a5c9eb5537f990
Piotr Kowalczuk <p.kowalczuk.priv@gmail.com>
2018-10-09 18:23:01 +0200

    .tmp renamed into tmp

--- .circleci/scripts/generate.sh
+++ .circleci/scripts/generate.sh
@@ -1,8 +1,8 @@
 : ${PROTOC:="/usr/local/bin/protoc"}
-: ${SCALAPBC:="./.tmp/scalapbc/scalapbc-0.8.1/bin/scalapbc"}
+: ${SCALAPBC:="./tmp/scalapbc/scalapbc-0.8.1/bin/scalapbc"}
 PROTO_INCLUDE="-I=/usr/include -I=."
 
-DIR_JAVA="./.tmp/java/"
+DIR_JAVA="./tmp/java/"
 DIR_PYTHON="./"
 
 case $1 in
@@ -13,9 +13,9 @@ case $1 in
         python -m grpc_tools.protoc ${PROTO_INCLUDE} --python_out=${DIR_PYTHON}ntypes *.proto
         ;;
     scala)
-        rm -rf ./.tmp/scala
-        mkdir -p ./.tmp/scala
-        ${SCALAPBC} -v361 --scala_out=./.tmp/scala/ntypes.jar ${PROTO_INCLUDE} *.proto
+        rm -rf ./tmp/scala
+        mkdir -p ./tmp/scala
+        ${SCALAPBC} -v361 --scala_out=./tmp/scala/ntypes.jar ${PROTO_INCLUDE} *.proto
         ;;
     golang | go)
         ${PROTOC} ${PROTO_INCLUDE} --go_out=${GOPATH}/src *.proto
--- .circleci/scripts/install_scalapbc.sh
+++ .circleci/scripts/install_scalapbc.sh
@@ -1,7 +1,7 @@
 curl -L https://github.com/scalapb/ScalaPB/releases/download/v0.8.1/scalapbc-0.8.1.zip > scalapbc.zip
 
-rm -rf ./.tmp/scalapbc
-mkdir -p ./.tmp/scalapbc
-unzip scalapbc.zip -d ./.tmp/scalapbc
+rm -rf ./tmp/scalapbc
+mkdir -p ./tmp/scalapbc
+unzip scalapbc.zip -d ./tmp/scalapbc
 
 # scalapbc has no version flag
