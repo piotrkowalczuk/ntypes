54d146c136c70309b61cd30a71be3f041b29719c
Piotr Kowalczuk <p.kowalczuk.priv@gmail.com>
2018-10-09 17:40:49 +0200

    scala pipeline instead of java #2

--- .circleci/scripts/install_scalapbc.sh
+++ .circleci/scripts/install_scalapbc.sh
@@ -1,7 +1,7 @@
 curl -L https://github.com/scalapb/ScalaPB/releases/download/v0.7.4/scalapbc-0.7.4.zip > scalapbc.zip
 
-rm -rf ./tmp/scalapbc
-mkdir -p ./tmp/scalapbc
-unzip scalapbc.zip -d ./tmp/scalapbc
+rm -rf ./.tmp/scalapbc
+mkdir -p ./.tmp/scalapbc
+unzip scalapbc.zip -d ./.tmp/scalapbc
 
 # scalapbc has no version flag
