8b11a7bd38d657f773e14095a8b41b08b5ce497e
Piotr Kowalczuk <p.kowalczuk.priv@gmail.com>
2024-09-28 18:51:01 +0200

    swift support (testing) (#22)

--- Package.swift
+++ Package.swift
@@ -3,6 +3,13 @@ import PackageDescription
 
 let package = Package(
     name: "ntypes",
+    platforms: [
+        .macOS(.v10_15), // Specify the minimum macOS version required
+        .iOS(.v13),
+        .watchOS(.v6),
+        .tvOS(.v13),
+        .visionOS(.v1)
+    ],
     products: [
         .library(
             name: "NTypes",
@@ -12,6 +19,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.28.1"),
         .package(url: "https://github.com/grpc/grpc-swift.git", from: "1.23.1"),
+        .package(url: "https://github.com/swiftlang/swift-testing.git", branch: "main"),
     ],
     targets: [
         // Główny target dla kodu źródłowego
@@ -37,11 +45,40 @@ let package = Package(
                 "LICENSE.txt",
                 "README.md",
                 "ntypes.proto",
+                "ntypes_test.pb.swift",
             ],
             sources: [
                 "ntypes.pb.swift",
             ],
             publicHeadersPath: ""
+        ),
+       .testTarget(
+            name: "NTypesTests",
+            dependencies: [
+                .target(name: "NTypes"),
+                .product(name: "Testing", package: "swift-testing"),
+            ],
+            path: ".",
+            exclude: [
+                "go.mod",
+                "go.sum",
+                "ntypes.go",
+                "ntypes.pb.go",
+                "ntypes_test.go",
+                "ntypespq",
+                "ntypes",
+                "Makefile",
+                "setup.py",
+                "setup.cfg",
+                "MANIFEST.in",
+                "LICENSE.txt",
+                "README.md",
+                "ntypes.proto",
+                "ntypes.pb.swift"
+            ],
+            sources: [
+                "ntypes_test.pb.swift"
+            ]
         ),
     ]
 )
---
+++ ntypes_test.pb.swift
@@ -0,0 +1,7 @@
+import NTypes
+import Testing
+
+@Test func helloWorld() {
+    let _ = NTypes.Ntypes_Bool()
+    #expect(Bool(true))
+}
